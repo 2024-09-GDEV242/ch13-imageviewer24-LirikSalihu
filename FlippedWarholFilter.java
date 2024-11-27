52f19c77343fd9a6373e946fd64ec5c5e378eacc
Lirik Salihu <G00304223@stu.raritanval.edu>
2024-11-26 22:08:27 -0500

    Create FlippedWarholFilter.java

---
+++ FlippedWarholFilter.java
@@ -0,0 +1,16 @@
+
+/**
+ * Creates a flipped Warhol-style image with mirrored tints.
+ *
+ * @author Lirik Salihu
+ * @version 2024.11.26
+ */
+public class FlippedWarholFilter extends Filter {
+    public FlippedWarholFilter() {
+        super("Flipped Warhol");
+    }
+
+    @Override
+    public void apply(OFImage image) {
+    }
+}
