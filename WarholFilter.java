dde9f57c33a3ea33ca3312d4e7ad5d83b23ce6ef
Lirik Salihu <G00304223@stu.raritanval.edu>
2024-11-26 22:08:25 -0500

    Create WarholFilter.java

---
+++ WarholFilter.java
@@ -0,0 +1,16 @@
+
+/**
+ * Creates a Warhol-style image with tinted quadrants.
+ *
+ * @author Lirik Salihu
+ * @version 2024.11.26
+ */
+public class WarholFilter extends Filter {
+    public WarholFilter() {
+        super("Warhol");
+    }
+
+    @Override
+    public void apply(OFImage image) {
+    }
+}
